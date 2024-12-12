ac75c35cf3ae3e10de395e915cb60755eee7c248
Matan Goldenberg <Matangoldi7@gmail.com>
2024-12-12 17:26:04 +0200

    Clean date base from unwanted files and up to date .gitgnore

--- app.py
+++
@@ -1,7 +0,0 @@
-"""Experimentation Area for Package Modules"""
-
-from package_name.logger import logger
-from package_name.tools.calculator import Calculator
-
-if __name__ == "__main__":
-    logger.info(f"1+3={Calculator.add(1,3)}")
